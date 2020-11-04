d77a79264efe6c45b1adeed2c5114efd3c664d20
Volodymyr <volodymyrquo@gmail.com>
2020-11-04 06:37:05 +0200

    done select country

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -3,14 +3,14 @@ import { fetchDailyData } from '../../api';
 import { Line, Bar } from 'react-chartjs-2';
 import styles from './Chart.module.css';
 
-const Chart = (props) => {
+const Chart = ({ data, country }) => {
   const [dailyData, setDailyData] = useState({});
 
   useEffect(() => {
     const fetchAPI = async () => {
       setDailyData(await fetchDailyData());
     };
-    console.log(dailyData);
+
     fetchAPI();
   });
   const lineChart = dailyData.length ? (
